ac13d464cc86ade48518aad058975499bf85072f
James Hubert <jwhubert91@gmail.com>
2022-10-21 20:11:31 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+
+// Set up tailwind with webpack: https://gist.github.com/bradtraversy/1c93938c1fe4f10d1e5b0532ae22e16a
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 9000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+}
