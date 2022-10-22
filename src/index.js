08c05e5595563affa976c746d938ac910232eb0a
James Hubert <jwhubert91@gmail.com>
2022-10-21 20:37:12 -0400

    add comment to find stimulus intro

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./style.css"
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
+// https://stimulus.hotwired.dev/handbook/hello-stimulus
+
 window.Stimulus = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
