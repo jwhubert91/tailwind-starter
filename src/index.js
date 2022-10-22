71916abc826ff7bb1ff078a2289f85bafa40e9e6
James Hubert <jwhubert91@gmail.com>
2022-10-21 20:35:51 -0400

    stimulus successfully added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
