f25a02b4dda7399aa2f1421e0d8f8327844018e7
James Hubert <jwhubert91@gmail.com>
2022-10-24 14:04:34 -0400

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      tablet: "601px",
+      md: "641px",
+      lg: "1100px",
+    },
+    colors: {
+      white: "var(--welcomeWhite)",
+      black: "var(--welcomeBlack)",
+      grayBlack: "var(--grayBlack)",
+      primary: "var(--primaryBlue)",
+      gray: "var(--lightGray)",
+    },
+    fontFamily: {
+      sans: ["Nutino-Sans"],
+      gilroy: ["Gilroy"],
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      spacing: {
+        md: "30px",
+        lg: "90px",
+      },
+    },
   },
   plugins: [],
 }
